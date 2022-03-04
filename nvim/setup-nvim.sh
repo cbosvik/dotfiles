30446773e2dde6988465051fce8db42d78c1e387
Christer Bosvik <50860904+bosvik@users.noreply.github.com>
2022-03-04 22:31:10 +0100

    Update setup-nvim.sh

--- nvim/setup-nvim.sh
+++ nvim/setup-nvim.sh
@@ -4,8 +4,8 @@ mkdir -p $NVIMDIR/lua
 mkdir -p $NVIMDIR/vscode
 mkdir -p $NVIMDIR/plug-config
 
-ln -sfr $DOTFILES/nvim/lua/* $NVIMDIR/lua
-ln -sfr $DOTFILES/nvim/vscode/* $NVIMDIR/vscode
-ln -sfr $DOTFILES/nvim/plug-config/* $NVIMDIR/plug-config
+ln -sf $DOTFILES/nvim/lua/* $NVIMDIR/lua
+ln -sf $DOTFILES/nvim/vscode/* $NVIMDIR/vscode
+ln -sf $DOTFILES/nvim/plug-config/* $NVIMDIR/plug-config
 ln -sf $DOTFILES/nvim/init.vim $NVIMDIR/init.vim
 
