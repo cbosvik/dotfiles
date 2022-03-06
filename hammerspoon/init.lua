2ffcccbd97f75322084e4c0467a944add2acbc5e
Christer Bosvik <christer.bosvik@skf.com>
2022-03-06 22:00:55 +0100

    phoenix

---
+++ hammerspoon/init.lua
@@ -0,0 +1,18 @@
+hs.hotkey.bind({"ctrl"}, "F1", function()
+  local app = hs.application.get("kitty")
+
+  if app then
+      if not app:mainWindow() then
+          app:selectMenuItem({"kitty", "New OS window"})
+      elseif app:isFrontmost() then
+          app:hide()
+      else
+          app:activate()
+      end
+  else
+      hs.application.launchOrFocus("kitty")
+      app = hs.application.get("kitty")
+  end
+
+  app:mainWindow():moveToUnit'[100,50,0,0]'
+end)
