ee682ed0cfb36a2c932f94a4792178e76c259406
bosvik <christer.bosvik@skf.com>
2023-05-02 16:04:57 +0200

    update config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,9 @@
 return {
   { "dracula/vim", name = "dracula" },
-  {
-    "LazyVim/LazyVim",
-    opts = { colorscheme = "dracula" },
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = { colorscheme = "dracula" },
+  -- },
   {
     "folke/tokyonight.nvim",
     lazy = true,
