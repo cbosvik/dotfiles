6c16a53627a18932dd87f25ab351c189b3539418
bosvik <christer.bosvik@skf.com>
2023-12-22 22:36:17 +0100

    add modules

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -7,6 +7,8 @@ return {
       require("neorg").setup({
         load = {
           ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+          ["core.integrations.nvim-cmp"] = {},
           ["core.concealer"] = {
             config = {
               icon_preset = "varied",
